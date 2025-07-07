68832fe2537b5efdd2d77ffa1e73fa168f4acb90
vijayramanABC <vijay.raman@adityabirlacapital.com>
2025-07-07 14:18:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,6 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return "Hello World from Vijay"
+    client = AzureOpenAI(azure_endpoint="https://vijay15apr2.openai.azure.com/", api_key="9mJmzN7n9NFtvK2qLca4tb1DsD0DWVQGVmNRVbITVrfnwete4yZpJQQJ99BDACfhMk5XJ3w3AAABACOGiAbm", api_version="2025-01-01-preview")
+    completion = client.chat.completions.create(model="gpt-4o", messages=[{"role": "user", "content": "Names of Ganpati"}])
+    return completion.choices[0].message.content
